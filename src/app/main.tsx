d309e7c2514e4d44ceb420e74a05c58fe5526a4e
Scott Becker <becker.scott@gmail.com>
2017-02-25 10:43:45 -0800

    Add examples of immutable records w/ typescript

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,4 +1,51 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
-ReactDOM.render(<div>Hello World!</div>, document.getElementById('root'))
+import {
+  recordify,
+  makeTypedFactory,
+  TypedRecord,
+} from 'typed-immutable-record'
+
+// create some TypeScript interfaces
+interface IPet {
+  name: string
+  type: string
+}
+
+interface IPerson {
+  name: string
+  pet?: IPet
+}
+
+// create some typed immutable records w/ 'TypedRecord'
+interface IPetRecord extends TypedRecord<IPetRecord>, IPet {}
+interface IPersonRecord extends TypedRecord<IPersonRecord>, IPerson {}
+
+// create a factory with property defaults w/ 'makeTypedFactory'
+const LukeRecordFactory = makeTypedFactory<IPerson, IPersonRecord>({
+  name: 'Luke',
+})
+
+// create some instances, overriding the defaults
+const master = LukeRecordFactory({
+  name: 'Master Luke',
+})
+
+const councilMaster = LukeRecordFactory({
+  name: 'Council Master Luke',
+})
+
+// create an pet instance child record w/ 'recordify', without prior defaults/factory
+const skywalkerRecord = recordify<IPerson, IPersonRecord>({
+  name: 'Luke Skywalker',
+  pet: recordify<IPet, IPetRecord>({
+    name: 'Artoo',
+    type: 'Droid',
+  })
+})
+
+// property autocompletion works, set() works, etc...
+const updatedSkywalker = skywalkerRecord.set('name', 'Duke Piewalker')
+
+ReactDOM.render(<div>{updatedSkywalker.name} - {updatedSkywalker.pet.name}</div>, document.getElementById('root'))
