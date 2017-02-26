b9fd29f7e531c31be219bd170ad2bb4f205af504
Scott Becker <becker.scott@gmail.com>
2017-02-26 01:31:29 -0800

    Add editorconfig and 4 space indentation

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,20 +3,20 @@ import * as ReactDOM from "react-dom";
 import App from "../components/app";
 
 import {
-  makeTypedFactory,
-  recordify,
-  TypedRecord,
+    makeTypedFactory,
+    recordify,
+    TypedRecord,
 } from "typed-immutable-record";
 
 // create some TypeScript interfaces
 interface IPet {
-  name: string;
-  type: string;
+    name: string;
+    type: string;
 }
 
 interface IPerson {
-  name: string;
-  pet?: IPet;
+    name: string;
+    pet?: IPet;
 }
 
 // create some typed immutable records w/ "TypedRecord"
@@ -25,27 +25,27 @@ interface IPersonRecord extends TypedRecord<IPersonRecord>, IPerson {}
 
 // create a factory with property defaults w/ "makeTypedFactory"
 const LukeRecordFactory = makeTypedFactory<IPerson, IPersonRecord>({
-  name: "Luke",
+    name: "Luke",
 });
 
 // create some instances, overriding the defaults
 const master = LukeRecordFactory({
-  name: "Master Luke",
+    name: "Master Luke",
 });
 
 // create an pet instance child record w/ "recordify", without prior defaults/factory
 const skywalkerRecord = recordify<IPerson, IPersonRecord>({
-  name: "Luke Skywalker",
-  pet: recordify<IPet, IPetRecord>({
-    name: "Artoo",
-    type: "Droid",
-  }),
+    name: "Luke Skywalker",
+    pet: recordify<IPet, IPetRecord>({
+        name: "Artoo",
+        type: "Droid",
+    }),
 });
 
 // property autocompletion works, set() works, etc...
 const updatedSkywalker = skywalkerRecord.set("name", "Duke Piewalker");
 
 ReactDOM.render(
-  <App message={`${master.name} - ${updatedSkywalker.pet && updatedSkywalker.pet.name}`} />,
-  document.getElementById("root"),
+    <App message={`${master.name} - ${updatedSkywalker.pet && updatedSkywalker.pet.name}`} />,
+    document.getElementById("root"),
 );
--- src/components/app.tsx
+++ src/components/app.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 // typed stateless component
 const App: React.StatelessComponent<{ message: string }> =
-  ({ message }) => <div>{message}</div>;
+    ({ message }) => <div>{message}</div>;
 App.displayName = "MyApp";
 
 export default App;
